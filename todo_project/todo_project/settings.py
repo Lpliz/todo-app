7d7de9fb6920f4ac74c09cedf8173c50573c1624
LIZA <106732894+Lpliz@users.noreply.github.com>
2025-01-09 20:56:13 +0530

    Update settings.py

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t7g5h#mv@awrpyl#!g00c05@6lukr5!@!6r8fih9@da^^s-*wq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
