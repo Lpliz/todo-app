1c208bfccc74a3ab1b091c52a267d968b3e67997
LIZA <106732894+Lpliz@users.noreply.github.com>
2025-01-09 19:54:42 +0530

    Update settings.py

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t7g5h#mv@awrpyl#!g00c05@6lukr5!@!6r8fih9@da^^s-*wq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
